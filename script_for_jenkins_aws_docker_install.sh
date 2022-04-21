c59d842222778686e96e5075d526ab11cb5bbd02
techay-mihir-simform <70096980+techay-mihir-simform@users.noreply.github.com>
2022-04-21 20:32:55 +0530

    Update script_for_jenkins_aws_docker_install.sh

--- script_for_jenkins_aws_docker_install.sh
+++ script_for_jenkins_aws_docker_install.sh
@@ -27,7 +27,8 @@ docker --version
 
 
 ####AWS CLI installation
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+sudo apt-get install unzip -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" \
+        unzip awscliv2.zip  \
+        sudo ./aws/install
 
